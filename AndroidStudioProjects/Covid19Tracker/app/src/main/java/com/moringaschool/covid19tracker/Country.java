c8d0514c8496cc48636bbf47de558808ac977622
Serahwakaba <serahwakaba9@gmail.com>
2021-07-05 15:09:47 +0300

    array commmit

--- AndroidStudioProjects/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/Country.java
+++ AndroidStudioProjects/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/Country.java
@@ -14,26 +14,26 @@ import android.widget.Toast;
 public class Country extends AppCompatActivity {
     private TextView mLocationTextView;
     private ListView mListView;
-//    private String[] restaurants = new String[] {"Algeria","Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon",
-//            "Cabo Verde", "Central African Republic", "Chad", "Comoros", "Congo", "the Democratic Republic of Congo",
-//            "Cote d'Ivoire", "Djibouti", "Equatorial Guinea", "Egypt", "Eritrea", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau"};
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_country);
-//        mListView = (ListView) findViewById(R.id.listView);
-//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-//
+    private String[] restaurants = new String[] {"Algeria","Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon",
+            "Cabo Verde", "Central African Republic", "Chad", "Comoros", "Congo", "the Democratic Republic of Congo",
+            "Cote d'Ivoire", "Djibouti", "Equatorial Guinea", "Egypt", "Eritrea", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
 //        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
 //        mListView.setAdapter(adapter);
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-//                String restaurant = ((TextView)view).getText().toString();
-//                Toast.makeText(Country.this, restaurant, Toast.LENGTH_LONG).show();
-//            }
-//        });
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(Country.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
 //
 //        Intent intent = getIntent();
 //        String location = intent.getStringExtra("location");
