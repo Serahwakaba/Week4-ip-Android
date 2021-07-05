248df39f0fd2fc217a75c69d28f96ae4fa9e5b1e
Serahwakaba <serahwakaba9@gmail.com>
2021-07-05 13:47:05 +0300

    covid19 tracker commit

---
+++ AndroidStudioProjects/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/Country.java
@@ -0,0 +1,42 @@
+package com.moringaschool.covid19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Country extends AppCompatActivity {
+//    private TextView mLocationTextView;
+//    private ListView mListView;
+//    private String[] restaurants = new String[] {"Algeria","Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cameroon",
+//            "Cabo Verde", "Central African Republic", "Chad", "Comoros", "Congo", "the Democratic Republic of Congo",
+//            "Cote d'Ivoire", "Djibouti", "Equatorial Guinea", "Egypt", "Eritrea", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau"};
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_country);
+//        mListView = (ListView) findViewById(R.id.listView);
+//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+//
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+//        mListView.setAdapter(adapter);
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                String restaurant = ((TextView)view).getText().toString();
+//                Toast.makeText(Country.this, restaurant, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        Intent intent = getIntent();
+//        String location = intent.getStringExtra("location");
+//        mLocationTextView.setText("Here are the countries affected by Covid19 in " + location);
+//    }
+//}
--- AndroidStudioProjects/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/MainActivity.java
+++ AndroidStudioProjects/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/MainActivity.java
@@ -2,13 +2,41 @@ package com.moringaschool.covid19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+//    private EditText mLocationEditText;
+//    private Button mFindCountrysButton;
+     @BindView(R.id.findCountriesButton) Button mFindCountrysButton;
+     @BindView(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+////        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+////        mFindCountrysButton = (Button) findViewById(R.id.mFindCountrysButton);
+//        mFindCountrysButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                String location = mLocationEditText.getText().toString();
+//                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this, Country.class);
+//                intent.putExtra("location", location);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 }
