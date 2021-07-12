c65daf65304edbd8a83c0d1b6328b7e58e8640bc
Serahwakaba <serahwakaba9@gmail.com>
2021-07-12 08:35:45 +0300

    connect retrofit commit

--- Desktop/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/MainActivity.java
+++ Desktop/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/MainActivity.java
@@ -1,56 +1,56 @@
-//package com.moringaschool.covid19tracker;
-//
-//import android.app.ProgressDialog;
-//import android.os.Bundle;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.recyclerview.widget.LinearLayoutManager;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.moringaschool.covid19tracker.adapter.CustomAdapter;
-//import com.moringaschool.covid19tracker.model.Countrymodel;
-//import com.moringaschool.covid19tracker.network.GetDataService;
-//import com.moringaschool.covid19tracker.network.RetrofitClientInstance;
-//
-//import java.util.List;
-//
-//import retrofit2.Call;
-//import retrofit2.Callback;
-//import retrofit2.Response;
-//
-//public class MainActivity extends AppCompatActivity {
-//    private CustomAdapter adapter;
-//    private RecyclerView recyclerView;
-//    ProgressDialog progressDoalog;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        progressDoalog = new ProgressDialog(MainActivity.this);
-//        progressDoalog.setMessage("Loading....");
-//        progressDoalog.show();
-//
-//        /*Create handle for the RetrofitInstance interface*/
-//        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-//        Call<List<Countrymodel>> call = service.getAllCountries();
-//        call.enqueue(new Callback<List<Countrymodel>>() {
-//            @Override
-//            public void onResponse(Call<List<Countrymodel>> call, Response<List<Countrymodel>> response) {
-//                progressDoalog.dismiss();
-//                generateDataList(response.body());
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Countrymodel>> call, Throwable t) {
-//                progressDoalog.dismiss();
-//                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-//
+package com.moringaschool.covid19tracker;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.covid19tracker.adapter.CustomAdapter;
+import com.moringaschool.covid19tracker.model.Countrymodel;
+import com.moringaschool.covid19tracker.network.GetDataService;
+import com.moringaschool.covid19tracker.network.RetrofitClientInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDoalog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDoalog = new ProgressDialog(MainActivity.this);
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<Countrymodel>> call = service.getAllCountries();
+        call.enqueue(new Callback<List<Countrymodel>>() {
+            @Override
+            public void onResponse(Call<List<Countrymodel>> call, Response<List<Countrymodel>> response) {
+                progressDoalog.dismiss();
+                generateDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Countrymodel>> call, Throwable t) {
+                progressDoalog.dismiss();
+                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 //    /*Method to generate List of data using RecyclerView with custom adapter*/
 //    private void generateDataList(List<Countrymodel> photoList) {
 //        recyclerView = findViewById(R.id.customRecyclerView);
@@ -58,5 +58,5 @@
 //        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
 //        recyclerView.setLayoutManager(layoutManager);
 //        recyclerView.setAdapter(adapter);
-//    }
-//}
+    }
+}
