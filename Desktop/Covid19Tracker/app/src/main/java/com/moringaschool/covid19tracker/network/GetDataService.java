19eda73449d3057a26633ef01e2fb77ae4dbd645
Serahwakaba <serahwakaba9@gmail.com>
2021-07-12 08:47:10 +0300

    getting data service commit

--- Desktop/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/network/GetDataService.java
+++ Desktop/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/network/GetDataService.java
@@ -1,14 +1,14 @@
-//package com.moringaschool.covid19tracker.network;
-//
-//import com.moringaschool.covid19tracker.model.Countrymodel;
-//
-//import java.util.List;
-//
-//import retrofit2.Call;
-//import retrofit2.http.GET;
-//
-//public interface GetDataService {
-//    @GET("/countries")
-//    Call<List<Countrymodel>> getAllCountries();
-//
-//}
+package com.moringaschool.covid19tracker.network;
+
+import com.moringaschool.covid19tracker.model.Countrymodel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+    @GET("/countries")
+    Call<List<Countrymodel>> getAllCountries();
+
+}
