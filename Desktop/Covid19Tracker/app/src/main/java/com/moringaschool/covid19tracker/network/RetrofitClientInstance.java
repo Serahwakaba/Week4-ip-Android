70b3b357bf4ed8e9638d0074d134d60d7bc67913
Serahwakaba <serahwakaba9@gmail.com>
2021-07-12 08:46:02 +0300

    retrofit commit

--- Desktop/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/network/RetrofitClientInstance.java
+++ Desktop/Covid19Tracker/app/src/main/java/com/moringaschool/covid19tracker/network/RetrofitClientInstance.java
@@ -1,20 +1,20 @@
-//package com.moringaschool.covid19tracker.network;
-//
-//import retrofit2.Retrofit;
-//import retrofit2.converter.gson.GsonConverterFactory;
-//
-//public class RetrofitClientInstance {
-//
-//    private static Retrofit retrofit;
-//    private static final String BASE_URL = "https://corona.lmao.ninja/v2/all/";
-//
-//    public static Retrofit getRetrofitInstance() {
-//        if (retrofit == null) {
-//            retrofit = new retrofit2.Retrofit.Builder()
-//                    .baseUrl(BASE_URL)
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .build();
-//        }
-//        return retrofit;
-//    }
-//}
+package com.moringaschool.covid19tracker.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://corona.lmao.ninja/v2/all/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
