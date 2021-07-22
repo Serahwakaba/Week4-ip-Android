65cf0c04e9bcd884ca11f4a40496bfb2a2e513c6
Serahwakaba <serahwakaba9@gmail.com>
2021-07-22 14:26:27 +0300

    gesture commit

--- app/src/main/java/com/moringaschool/covid19tracker/SplashActivity.java
+++ app/src/main/java/com/moringaschool/covid19tracker/SplashActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
     Handler handler;
@@ -23,5 +27,37 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         },5000);
-    }
+
+            final GestureDetector gdt = new GestureDetector(new GestureListener());
+            final ImageView imageView  = (ImageView) findViewById(R.id.image_view);
+            imageView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(final View view, final MotionEvent event) {
+                    gdt.onTouchEvent(event);
+                    return true;
+                }
+            });
+        }
+
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    return false; // Right to left
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    return false; // Left to right
+                }
+
+                if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    return false; // Bottom to top
+                }  else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                    return false; // Top to bottom
+                }
+                return false;
+            }
+        }
+
 }
