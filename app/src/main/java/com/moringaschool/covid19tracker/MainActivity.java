5b519d1610a9afa74657874149ac514adf974513
Serahwakaba <serahwakaba9@gmail.com>
2021-07-23 10:05:59 +0300

    implement gesture detector methods

--- app/src/main/java/com/moringaschool/covid19tracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/covid19tracker/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -117,6 +118,36 @@ public class MainActivity extends AppCompatActivity  implements GestureDetector.
 
         startActivity(new Intent(getApplicationContext(), AffectedCountries.class));
 
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 
 //    public void goSave(View view) {
